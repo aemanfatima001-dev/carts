ac5b19cdfa061d7c5672897d673d29fd6d14aeaf
aemanfatima001-dev <aemanfatima001@gmail.com>
2026-04-21 20:54:35 +0500

    Resolve import error for App.jsx in main.jsx
    Fix unresolved import error for App component in main.jsx.

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+vite v8.0.6 building client environment for production...
+
+transforming...✓ 4 modules transformed.
+✗ Build failed in 39ms
+error during build:
+Build failed with 1 error:
+[UNRESOLVED_IMPORT] Error: Could not resolve './App.jsx' in src/main.jsx
+   ╭─[ src/main.jsx:3:17 ]
+   │
+ 3 │ import App from "./App.jsx";
