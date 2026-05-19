3ac29d61a47f5ec9ab4daad9a5637b6bea2936ab
aemanfatima001-dev <aemanfatima001@gmail.com>
2026-05-19 19:46:17 +0500

    Fix

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.BASE_URL ,
+  base: process.env.BASE_URL || '/menuuu'
 })
 
